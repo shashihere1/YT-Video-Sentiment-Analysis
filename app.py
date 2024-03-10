7de15dc1c87fc867364ab4be0726fce1bf11cede
Shashi Prakash <mail2shashi123456@gmail.com>
2024-03-10 15:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,3 +48,5 @@ def index():
         return render_template('result.html', sentiment_counts=sentiment_counts)
     return render_template('sentiment.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
