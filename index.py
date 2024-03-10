e0c057dba03e3e248d99e83d6ec19a67c6023cca
Shashi Prakash <mail2shashi123456@gmail.com>
2024-03-10 14:58:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-from wsgi import app
+from app import app
+
+# This might be where you have additional logic or routes specific to 'index'
+# For example:
+@app.route('/index')
+def index():
+    return 'This is the index page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
